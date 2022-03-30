bb34e529d5568c4e571e68f769663f00a3d972d6
Joshua Morony <joshua.morony@gmail.com>
2022-03-30 20:02:06 +1030

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dumb-loading-skeletons',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
